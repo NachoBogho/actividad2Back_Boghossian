8e96bc10588026713ca9ace2b203d83dd7563378
Ignacio Boghossian <boghossianignacio@gmail.com>
2023-10-04 22:31:56 -0300

    correccion

--- app.js
+++ app.js
@@ -3,63 +3,61 @@ const fs = require('fs').promises;
 class ProductManager {
   constructor(filePath) {
     this.path = filePath;
-    this.products = null;
   }
 
   loadProducts = async () => {
     try {
       const data = await fs.readFile(this.path, 'utf-8');
-      this.products = JSON.parse(data);
+      return JSON.parse(data);
     } catch (error) {
-      this.products = [];
+      return [];
     }
   }
 
-  saveProducts = async () => {
-    await fs.writeFile(this.path, JSON.stringify(this.products, null, 2), 'utf-8');
+  saveProducts = async (products) => {
+    await fs.writeFile(this.path, JSON.stringify(products, null, 2), 'utf-8');
   }
 
   addProduct = async (product) => {
-    await this.loadProducts();
+    let products = await this.loadProducts();
 
-    const lastId = this.products.length > 0 ? this.products[this.products.length - 1].id : 0;
+    const lastId = products.length > 0 ? products[products.length - 1].id : 0;
     product.id = lastId + 1;
 
-    this.products.push(product);
-    await this.saveProducts();
+    products.push(product);
+    await this.saveProducts(products);
   }
 
   getProducts = async () => {
-    await this.loadProducts();
-    return this.products;
+    return await this.loadProducts();
   }
 
   getProductById = async (id) => {
-    await this.loadProducts();
-    const product = this.products.find((p) => p.id === id);
+    const products = await this.loadProducts();
+    const product = products.find((p) => p.id === id);
     return product || null;
   }
 
-  updateProduct = async (id, updatedProduct) => {
-    await this.loadProducts();
+  updateProduct = async (id, modifications) => {
+    let products = await this.loadProducts();
 
-    const index = this.products.findIndex((p) => p.id === id);
+    const index = products.findIndex((p) => p.id === id);
     if (index !== -1) {
-      updatedProduct.id = this.products[index].id;
-      this.products[index] = updatedProduct;
-      await this.saveProducts();
-      return true;
+      products[index] = { ...products[index], ...modifications };
+      await this.saveProducts(products);
+      return products[index];
+    } else {
+      return "Producto no encontrado";
     }
-    return false;
   }
 
   deleteProduct = async (id) => {
-    await this.loadProducts();
+    let products = await this.loadProducts();
 
-    const index = this.products.findIndex((p) => p.id === id);
+    const index = products.findIndex((p) => p.id === id);
     if (index !== -1) {
-      this.products.splice(index, 1);
-      await this.saveProducts();
+      products.splice(index, 1);
+      await this.saveProducts(products);
       return true;
     }
     return false;
